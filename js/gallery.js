0e1874f0fe139cf8d06b57ea3dd3d5f0af7bf39c
Claudio Ludovico <ludo237@users.noreply.github.com>
2015-06-10 11:42:09 +0200

    Removed

--- js/gallery.js
+++ js/gallery.js
@@ -22,13 +22,8 @@
         },
 
         methods: {
-<<<<<<< HEAD
             // When click on an anchor of image this will be triggered
-            viewImage: function () {
-=======
-            // When click on an anchor of image this will triggered
             viewImage: function (event) {
->>>>>>> origin/master
                 // TODO: This seems utter bullshit, refactor this pls
                 this.next = event.target.parentNode.nextElementSibling;
                 this.previous = event.target.parentNode.previousElementSibling;
