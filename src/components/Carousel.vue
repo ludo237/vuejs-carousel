a2a1f2f4d6a6abd3f516d12ba32853d0b230ec48
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2016-11-29 16:22:37 +0100

    Improve Carousel.vue component
    This is a critical patch that fix a lot of small problems of the components template and script file. The CSS is still the same but that doesn't mean that will remain like that for ever.
    
    Patch notes:
    
    - Add `.prevent`  event modifier to `click` event in order to avoid any change to the URL
    - Add some comments to the script that describe some macro zones of it.
    - Change the method `selectThisPhoto` now it can be used in a more general way
    - Remove code duplication closing issue #5
    - Improve the quality of the code in general even inside the computed properties.

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,7 +4,7 @@
             <ul class="PhotoGallery__list">
                 <li class="PhotoGallery__item" v-for="(photo, index) in photos">
                     <figure class="PhotoGallery__figure">
-                        <img class="PhotoGallery__image" :src="photo.thumbnailUrl" @click="selectThisPhoto(photo, index)" />
+                        <img class="PhotoGallery__image" :src="photo.thumbnailUrl" @click.prevent="selectThisPhoto(photo, index)" />
                     </figure>
                 </li>
             </ul>
@@ -18,12 +18,12 @@
                         </figure>
                         <div class="Theater__commands">
                             <div class="Theater__command">
-                                <a href="#previous" v-show="isFirstPhoto" @click="previousPhoto(selectedPhoto.index)">
+                                <a href="#previous" v-show="!isFirstPhoto" @click.prevent="previousPhoto(selectedPhoto.index)">
                                     <i class="fa fa-fw fa-lg fa-4x fa-angle-double-left" aria-hidden="true"></i>
                                 </a>
                             </div>
                             <div class="Theater__command">
-                                <a href="#next" v-show="isLastPhoto" @click="nextPhoto(selectedPhoto.index)">
+                                <a href="#next" v-show="!isLastPhoto" @click.prevent="nextPhoto(selectedPhoto.index)">
                                     <i class="fa fa-fw fa-lg fa-4x fa-angle-double-right" aria-hidden="true"></i>
                                 </a>
                             </div>
@@ -34,7 +34,7 @@
                     <div class="Theater__header">
                         <h4>{{ selectedPhoto.title }}</h4>
                         <div class="Theater__command Theater__command--header">
-                            <a href="#close-theater" @click="closeTheater">
+                            <a href="#close-theater" @click.prevent="closeTheater">
                                 <i class="fa fa-fw fa-lg fa-times" aria-hidden="true"></i>
                             </a>
                         </div>
@@ -47,14 +47,33 @@
     </div>
 </template>
 <script>
-    export default {
+export default {
+
+        // Main source, this should be an API that returns a JSON of photos
         props: ["source"],
+
+        /**
+         * This is all the data we need for the gallery
+         * photos will store each photo from the api call
+         * and selectedPhoto will store an object that
+         * represent the current selected photo from the gallery.
+         *
+         * @returns {{photos: {}, selectedPhoto: {}}}
+         */
         data() {
             return {
                 photos: {},
                 selectedPhoto: {},
             }
         },
+
+        /**
+         * Once the application is ready we call the api in order
+         * to get the current list of photos. At the moment it's very
+         * limited to a simple GET api
+         *
+         * TODO: I think that here is were we should normalize the data from the API
+         */
         mounted() {
             this.$http.get(this.source).then((fetchedPhotos) => {
                 this.photos = fetchedPhotos.data;
@@ -64,41 +83,54 @@
         },
 
         methods: {
-            selectThisPhoto(photo, index) {
+
+            /**
+             * This is the main method that takes the selected photo
+             * and it will automatically shows up the theater and such
+             * @param photo
+             * @param index
+             */
+            selectThisPhoto(photo, index = null) {
                 this.selectedPhoto = photo;
-                this.selectedPhoto.index = index;
-                document.body.style.overflow = "hidden";
+
+                if (index !== null) {
+                    this.selectedPhoto.index = index;
+                }
             },
+
             previousPhoto(index) {
-                index = index - 1;
-                this.selectedPhoto = this.photos[index];
-                this.selectedPhoto.index = index;
+                index--;
+                this.selectThisPhoto(this.photos[index], index);
             },
+
             nextPhoto(index) {
-                index = index + 1;
-                this.selectedPhoto = this.photos[index];
-                this.selectedPhoto.index = index;
+                index++;
+                this.selectThisPhoto(this.photos[index], index);
             },
+
             closeTheater() {
-                this.selectedPhoto = {};
-                document.body.style.overflow = "auto";
+                this.selectThisPhoto({});
             },
         },
 
         computed: {
+
             isPhotoSelected() {
                 return Object.keys(this.selectedPhoto).length !== 0 && this.selectedPhoto.constructor === Object;
             },
+
             isTheaterClose() {
                 return {
-                    "Theater--closed": Object.keys(this.selectedPhoto).length === 0 && this.selectedPhoto.constructor === Object,
+                    "Theater--closed": !this.isPhotoSelected,
                 }
             },
+
             isFirstPhoto() {
-                return this.selectedPhoto.index != 0;
+                return this.selectedPhoto.index == 0;
             },
+
             isLastPhoto() {
-                return this.selectedPhoto.index != this.photos.length - 1;
+                return this.selectedPhoto.index == this.photos.length - 1;
             },
         }
     }
