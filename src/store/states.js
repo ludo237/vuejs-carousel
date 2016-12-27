9fcd176f810f05788e73a18f3b54e38d581354e5
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2016-12-27 11:00:44 +0100

    Improve comments on state.js

--- src/store/states.js
+++ src/store/states.js
@@ -4,12 +4,16 @@ export const state = {
      * Each time a photo is selected from
      * the gallery this property became a
      * Photo Object
+     * 
+     * @type {Object}
      */
     selectedPhoto: {},
 
     /**
      * List of photos available inside
      * the gallery.
+     * 
+     * @type {Array}
      */
     photos: []
 };
