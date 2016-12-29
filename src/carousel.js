c0abea4100e9d453680f39c14b5b30f9e4e686f5
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2016-12-29 20:08:57 +0100

    Remove debug code from the main entry file

--- src/carousel.js
+++ src/carousel.js
@@ -1,12 +1,7 @@
 import Vue from "vue";
-import VueResource from "vue-resource";
 import {store} from "./store/store";
-
 import Photos from "./components/Photos.vue";
 import Theater from "./components/Theater.vue";
-
-Vue.use(VueResource);
-Vue.config.debug = true;
 
 const carousel = new Vue({
     el: "#your-application-id",
@@ -17,11 +12,4 @@ const carousel = new Vue({
         Photos,
         Theater
     },
-
-    mounted() {
-        // Debug purpose
-        if (Vue.config.debug) {
-            console.log("Debug is on");
-        }
-    }
 });
